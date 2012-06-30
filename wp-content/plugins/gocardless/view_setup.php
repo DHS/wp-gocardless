3eb02b5e7ee6e2c1d61dd284315c0cfc4c195bfd
Elliott Stocks <elliottstocks@hotmail.co.uk>
2012-06-30 22:08:32 +0200

    Add classes to input boxes, re-order API details to match order in GoCardless API Keys. Add id to sandbox label.

--- wp-content/plugins/gocardless/view_setup.php
+++ wp-content/plugins/gocardless/view_setup.php
@@ -1,4 +1,3 @@
-
 <h2>Setup</h2>
 
 <h3>1. Set up your API keys</h3>
@@ -9,18 +8,18 @@ up</a> for a GoCardless merchant account.
 </p>
 
 <p>Update the Redirect URI in the GoCardless Developer tab to point to any page or post on your website such as:</p>
-<input type="text" readonly="true" value="<?php echo site_url(); ?>" />
+<input type="text" readonly="true" class="regular-text" value="<?php echo site_url(); ?>" />
 
 <p>Copy your API keys from the GoCardless Developer tab and paste them in below.</p>
 
 <form action="" method="post">
 <input type="hidden" name="form" value="config" />
 <table>
-<tr><td>App ID</td><td><input type="text" name="app_id" value="<?php echo $gocardless_config['app_id']; ?>" /></td></tr>
-<tr><td>App secret</td><td><input type="text" name="app_secret" value="<?php echo $gocardless_config['app_secret']; ?>" /></td></tr>
+<tr><td>App ID</td><td><input type="text" name="app_id" class="regular-text" value="<?php echo $gocardless_config['app_id']; ?>" /></td></tr>
+<tr><td>App secret</td><td><input type="text" name="app_secret" class="regular-text" value="<?php echo $gocardless_config['app_secret']; ?>" /></td></tr>
+<tr><td>Access token</td><td><input type="text" name="access_token" class="regular-text" value="<?php echo $gocardless_config['access_token']; ?>" /></td></tr>
 <tr><td>Merchant ID</td><td><input type="text" name="merchant_id" value="<?php echo $gocardless_config['merchant_id']; ?>" /></td></tr>
-<tr><td>Access token</td><td><input type="text" name="access_token" value="<?php echo $gocardless_config['access_token']; ?>" /></td></tr>
-<tr><td><input type="checkbox" name="sandbox" id="sandbox" <?php if ($gocardless_config['sandbox']) { echo 'checked'; } ?>/> <label for="">Sandbox account</label></td></tr>
+<tr><td><input type="checkbox" name="sandbox" id="sandbox" <?php if ($gocardless_config['sandbox']) { echo 'checked'; } ?>/> <label for="sandbox">Sandbox account</label></td></tr>
 <tr><td colspan="2" class="submit"><input type="submit" name="submit" value="Update API keys &raquo;" /></td></tr>
 </table>
 </form>
